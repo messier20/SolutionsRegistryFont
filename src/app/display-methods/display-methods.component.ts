336fcb883adede7718fa87b28eceb24ec5dd6287
monikossite <monikossite@gmail.com>
2018-06-04 23:25:49 +0300

    code clean up

--- src/app/display-methods/display-methods.component.ts
+++ src/app/display-methods/display-methods.component.ts
@@ -1,8 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {BackendServiceService} from "../backend-service/backend-service.service";
 import {Method} from "../models/method";
-import {FormBuilder, FormGroup, Validator, Validators} from "@angular/forms";
-import {MatSelectModule} from '@angular/material/select';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {SolutionWithReagents} from "../models/SolutionWithReagents";
 import {Reagent} from "../models/Reagent";
 import {Solution} from "../models/Solution";
@@ -25,17 +24,12 @@ export class DisplayMethodsComponent implements OnInit {
 
   methods: Method[] = [];
   selectedMethod: Method;
-  // ObjectId = require('mongodb').ObjectId;
-  methodId: string;
   methodTitle;
   solution: Solution;
   id: string;
   currentReagents: Reagent[] = [];
   inputForm: FormGroup;
-  foods = [];
-  methodDetails = [];
   solutionsWithReagents: SolutionWithReagents[] = [];
-  reagentsForSolution = [];
   solutionsHistoryEntry;
 
   constructor(private backend: BackendServiceService,
@@ -46,7 +40,7 @@ export class DisplayMethodsComponent implements OnInit {
     this.getAllMethods();
     this.createInputForm();
     this.checkSetting();
-    // this.getFullMethodById();
+
   }
 
 
@@ -60,16 +54,9 @@ export class DisplayMethodsComponent implements OnInit {
       temporaryMethods.forEach(data => {
         console.log(data);
         this.methods.push(data);
-        console.log("data methods", this.methods);
-
       });
     });
 
-    this.foods = [
-      {value: 'steak-0', viewValue: 'Steak'},
-      {value: 'pizza-1', viewValue: 'Pizza'},
-      {value: 'tacos-2', viewValue: 'Tacos'}
-    ];
 
   }
 
@@ -78,93 +65,48 @@ export class DisplayMethodsComponent implements OnInit {
       methodTitle: ['', [Validators.required]],
       solutionTitle: ['', [Validators.required]],
       solutionCreationDate: ['', [Validators.required]]
-      // methodId: ''
 
     });
   }
 
   checkSetting() {
-    console.log("before if", this.inputForm.get('methodTitle').value);
     if (this.inputForm.get('methodTitle').value !== null) {
-      console.log("in if", this.inputForm.get('methodTitle').value);
     }
   }
 
-  setMethod() {
-    this.methodTitle = this.inputForm.get('methodTitle').value;
-    // this.getFullMethodById();
-
-    // let method = this.inputForm.get('methodTitle').value;
-    // console.log("in set", method);
-  }
 
   getFullMethodById() {
-    console.log('inside');
     let selectedMethodTitle = this.inputForm.get('methodTitle').value;
-    console.log('method title', selectedMethodTitle)
     for (let method1 of this.methods) {
-      console.log('one method', method1);
       if (method1.methodTitle === selectedMethodTitle) {
-        console.log('method.methodTitle', method1.methodTitle);
         this.selectedMethod = method1;
-        console.log('selected method', this.selectedMethod);
         this.id = method1.id;
-        console.log('this method id', this.id);
-        // this.inputForm.get('methodId').setValue(method1.methodId);
-        // console.log('method.id', this.inputForm.get('methodId').value);
         this.backend.getFullMethodById(this.id).then((data: IResponse) => {
           this.solutionsWithReagents = data.solutionWithReagentsList;
-          console.log('data', data);
-          console.log('solutions with reagents', this.solutionsWithReagents);
-          this.solutionsWithReagents.forEach(el => {
-            console.log('solution', el.solution);
-            console.log('aa');
-          });
         });
       }
     }
-
 
   }
 
   setSolutionDetails() {
     this.currentReagents = [];
     this.solution = null;
-    console.log('currentReagents', this.currentReagents);
     let selectedSolution = this.inputForm.get('solutionTitle').value;
     for (let solutionWithReagents of this.solutionsWithReagents) {
       if (solutionWithReagents.solution.solutionTitle === selectedSolution) {
         this.solution = solutionWithReagents.solution;
         solutionWithReagents.reagentsList.forEach(data => {
           this.currentReagents.push(data);
-          console.log('data', data);
-          console.log('current  Reagents', this.currentReagents);
         });
-        console.log('solution', this.solution);
-        // this.reagentsForSolution = solutionWithReagents.reagentsList;
-        // this.reagentsForSolution.forEach(data => {
-        //
-        // });
-        // console.log('reagents list', this.reagentsForSolution);
+
       }
     }
   }
 
   setHistoryEntry() {
-    // this.solutionsHistoryEntry = undefined;
     if (this.inputForm.valid) {
-      console.log('inside valid form');
-      console.log('date', this.inputForm.get('solutionCreationDate').value);
-      // console.log('should be empty', this.solutionsHistoryEntry.solutionCreationDate);
-      // this.solutionsHistoryEntry.solutionCreationDate = this.inputForm.get('solutionCreationDate').value;
-      // console.log('should be date', this.solutionsHistoryEntry.solutionCreationDate);
-      // console.log(this.solutionsHistoryEntry.solution);
-      // this.solutionsHistoryEntry = new SolutionsHistory(this.solution, this.methodTitle, this.currentReagents);
-      this.solutionsHistoryEntry = new SolutionsHistory(this.selectedMethod, this.solution, this.currentReagents, this.inputForm.get('solutionCreationDate').value)
-      // this.solutionsHistoryEntry.solution(this.solution);
-      // this.solutionsHistoryEntry.methodTitle(this.inputForm.get('methodTitle').value);
-      // this.solutionsHistoryEntry.reagentsList(this.currentReagents);
-      console.log('history entry', this.solutionsHistoryEntry);
+      this.solutionsHistoryEntry = new SolutionsHistory(this.selectedMethod, this.solution, this.currentReagents, this.inputForm.get('solutionCreationDate').value);
     }
   }
 
@@ -174,9 +116,8 @@ export class DisplayMethodsComponent implements OnInit {
     this.setHistoryEntry();
 
     this.backend.createNewEntry(this.solutionsHistoryEntry).then(data => {
-    console.log('yay', data);
+      console.log('yay', data);
     })
   };
-
 
 }
--- src/app/models/SolutionsHistory.ts
+++ src/app/models/SolutionsHistory.ts
@@ -17,54 +17,5 @@ export class SolutionsHistory {
     this.solutionCreationDate = solutionCreationDate;
   }
 
-//
-//   get method(): Method {
-//     return this.method;
-//   }
-//
-//   set method(value: Method) {
-//     this.method = value;
-//   }
-//
-//   get solutionCreationDate(): Date {
-//     return this.solutionCreationDate;
-//   }
-//
-//   set solutionCreationDate(value: Date) {
-//     this.solutionCreationDate = value;
-//   }
-//
-// // constructor(solutionsHistory?: SolutionsHistory) {
-//   //   if (solutionsHistory) {
-//   //     this._methodTitle = solutionsHistory._methodTitle;
-//   //     this._solution = solutionsHistory._solution;
-//   //     this._reagentsList = solutionsHistory._reagentsList;
-//   //     // this.solutionCreationDate = solutionsHistory.solutionCreationDate;
-//   //   }
-//   // }
-//
-//
-//   // get methodTitle(): string {
-//   //   return this._methodTitle;
-//   // }
-//   //
-//   // set methodTitle(value: string) {
-//   //   this._methodTitle = value;
-//   // }
-//
-//   get solution(): Solution {
-//     return this.solution;
-//   }
-//
-//   set solution(value: Solution) {
-//     this.solution = value;
-//   }
-//
-//   get reagentsList(): Reagent[] {
-//     return this.reagentsList;
-//   }
-//
-//   set reagentsList(value: Reagent[]) {
-//     this.reagentsList = value;
-//   }
+
 }
